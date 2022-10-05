a44984638fc2415c5a0b44d7fcb42be1c7199f95
Eltryo <david.merkl.dm@gmail.com>
2022-10-05 21:41:58 +0200

    Refactoring

--- lib/assets/colors.dart
+++ lib/assets/colors.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static const Color whiteBackgroundColor = Colors.white;
-  static const Color blackAppTextColor = Colors.black54;
+  static const Color blackAppTextColor = Colors.black;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sports_app/assets/colors.dart';
 
+import 'home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,40 +15,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "App",
       theme: ThemeData(
-          scaffoldBackgroundColor: AppTheme.whiteBackgroundColor,
-          textTheme: Theme.of(context).textTheme.apply(
-                bodyColor: AppTheme.blackAppTextColor,
-                displayColor: AppTheme.blackAppTextColor,
-              ),
-          appBarTheme: const AppBarTheme(
-              backgroundColor: AppTheme.whiteBackgroundColor,
-              titleTextStyle: TextStyle(
-                  color: AppTheme.blackAppTextColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500))),
-      home: const _HomePage(),
-    );
-  }
-}
-
-class _HomePage extends StatefulWidget {
-  const _HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<_HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<_HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("App"),
-        elevation: 0,
+        scaffoldBackgroundColor: AppTheme.whiteBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: AppTheme.blackAppTextColor,
+              displayColor: AppTheme.blackAppTextColor,
+            ),
       ),
-      body: const Center(
-        child: Text("Hello World"),
-      ),
+      home: const HomePage(),
     );
   }
 }
