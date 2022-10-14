32d9f64d7dc2bcfaf09b76ac09b7661bbc5003a3
Eltryo <david.merkl.dm@gmail.com>
2022-10-15 00:36:43 +0200

    Add user and user preferences

---
+++ lib/utils/user_preferences.dart
@@ -0,0 +1,11 @@
+import 'package:sports_app/model/user.dart';
+
+class UserPreferences{
+  static const User myUser = User(
+      imagePath: "lib/images/prison_mike.jpg",
+      username: "Prison_Mike",
+      email: "prison.mike@gmail.com",
+      about: "I stole and I robbed and I kidnapped the president's son an held him for ransom",
+      isDarkMode: false
+  );
+}
