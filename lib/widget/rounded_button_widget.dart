c25519979e75a3c3d7fde9486e9ef8f157f9eadc
Eltryo <david.merkl.dm@gmail.com>
2022-10-19 23:56:45 +0200

    Outsource ElevatedButton

---
+++ lib/widget/rounded_button_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class RoundedButtonWidget extends StatelessWidget {
+  final Color color;
+  final String text;
+
+  const RoundedButtonWidget({
+    required this.color,
+    required this.text,
+    Key? key
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () async {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        shape: const StadiumBorder(),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
