5503334afb9eeabfe70bb8869565c7eeb84ef936
Eltryo <david.merkl.dm@gmail.com>
2022-11-05 14:02:30 +0100

    Add registration form (wip)

--- lib/page/email_password_registration.dart
+++ lib/page/email_password_registration.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sports_app/services/auth.dart';
 import 'package:sports_app/widget/rounded_button_widget.dart';
 
 import '../utils/colors.dart';
@@ -12,7 +13,10 @@ class EmailPasswordRegistration extends StatefulWidget {
 }
 
 class _EmailPasswordRegistrationState extends State<EmailPasswordRegistration> {
-  final _formKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final AuthService _auth = AuthService();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -27,26 +31,58 @@ class _EmailPasswordRegistrationState extends State<EmailPasswordRegistration> {
             child: Form(
               key: _formKey,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter a username";
-                      }
-                      return null;
-                    },
+
+                  const Text(
+                      "Registration",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold
+                      ),
                   ),
+
+                  const SizedBox(height: 10),
+
+                  //form field for email
+                  buildTextFormField("email", emailController),
+
+                  const SizedBox(height: 10),
+
+                  //form field for password
+                  buildTextFormField("password", passwordController),
+
+                  const SizedBox(height: 10),
+
                   RoundedButtonWidget(
                       text: "Submit",
                       onPressed: () {
                         if (_formKey.currentState!.validate()) {
                           ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                  content: Text("Processing Data")));
+                              const SnackBar(content: Text("Processing Data")));
                         }
+
+                        _auth.createAccount(emailController.text, passwordController.text);
                       })
                 ],
               ),
             )));
+  }
+
+  TextFormField buildTextFormField(String hint, TextEditingController controller) {
+    return TextFormField(
+      controller: controller,
+      validator: (value) { //TODO: customize validator for each TextFormField
+        if (value == null || value.isEmpty) {
+          return "Please enter a $hint";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          hintText: "Enter your $hint",
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 10, horizontal: 10)),
+    );
   }
 }
