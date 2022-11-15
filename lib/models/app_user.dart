c948da966d8e166ccd607303add8fbbc267f24ab
Eltryo <david.merkl.dm@gmail.com>
2022-11-15 23:40:58 +0100

    Add user model

--- lib/models/app_user.dart
+++ lib/models/app_user.dart
@@ -1,5 +1,43 @@
-class AppUser {
-  final String uid;
+import 'package:equatable/equatable.dart';
 
-  AppUser({required this.uid});
+class AppUser extends Equatable {
+  final String uid;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String imageUrl;
+
+  const AppUser(
+      {required this.uid,
+      required this.firstName,
+      required this.lastName,
+      required this.email,
+      required this.imageUrl});
+
+  factory AppUser.fromJson(Map<String, dynamic> json) {
+    return AppUser(
+        uid: json["uid"] ?? "",
+        firstName: json["firstName"] ?? "",
+        lastName: json["lastName"] ?? "",
+        email: json["email"] ?? "",
+        imageUrl: json["imageUrl"] ?? "");
+  }
+
+  factory AppUser.empty() {
+    return const AppUser(
+        uid: "", firstName: "", lastName: "", email: "", imageUrl: "");
+  }
+
+  @override
+  List<Object?> get props => [uid, firstName, lastName, email, imageUrl];
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      "uid": uid,
+      "firstName": firstName,
+      "lastName": lastName,
+      "email": email,
+      "imageUrl": imageUrl
+    };
+  }
 }
