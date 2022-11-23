207303a55485a69a4034ca777fc77d2579f40e5d
Eltryo <david.merkl.dm@gmail.com>
2022-11-23 11:53:27 +0100

    Add logic to store user in firestore database

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,38 +1,44 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:sports_app/models/app_user.dart';
+import 'package:sports_app/models/profile_data.dart';
+import 'package:sports_app/services/firestore.dart';
 
 class AuthService {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirestoreService _firestoreService = FirestoreService();
 
   //Create user object based on FirebaseUser
-  AppUser? _mapFirebaseUser(User? user) {
-    return user != null
-        ? AppUser(
-            uid: user.uid, firstName: "", lastName: "", email: "", imageUrl: "")
-        : null; //TODO: build user api
+  AppUser? _mapToAppUser(User? user) {
+    return user != null ? AppUser(uid: user.uid) : null;
   }
 
   //Get user stream on auth change
   Stream<AppUser?> get user {
-    return _auth.authStateChanges().map(_mapFirebaseUser);
+    return _firebaseAuth.authStateChanges().map(_mapToAppUser);
   }
 
   //sign out
   Future<void> signOut() async {
     try {
-      return await _auth.signOut();
+      return await _firebaseAuth.signOut();
     } on FirebaseAuthException catch (e) {
       debugPrint(e.toString());
     }
   }
 
   //create account with email and password
-  Future createAccount(String email, String password) async {
+  Future createAccount(String email, String password, String username) async {
     try {
-      UserCredential userCredential = await _auth
-          .createUserWithEmailAndPassword(email: email, password: password);
-      return _mapFirebaseUser(userCredential.user);
+      await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((userCredential) {
+        //TODO think of a better identifier for profileDate
+        ProfileData profileData = ProfileData(
+            uid: userCredential.user!.uid, username: username, email: email);
+        _firestoreService.addUser(profileData);
+        return _mapToAppUser(userCredential.user);
+      });
     } on FirebaseAuthException catch (e) {
       if (e.code == "weak-password") {
         debugPrint("The password provided is too weak");
@@ -51,9 +57,9 @@ class AuthService {
   //sign in with email and password
   Future signInToAccount(String email, String password) async {
     try {
-      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-          email: email, password: password);
-      return _mapFirebaseUser(userCredential.user);
+      await _firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password)
+          .then((value) => _mapToAppUser(value.user));
     } on FirebaseAuthException catch (e) {
       if (e.code == "user-not-found") {
         debugPrint("No user found for that email");
--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,21 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
+import '../models/profile_data.dart';
+
 class FirestoreService {
   static final FirebaseFirestore db = FirebaseFirestore.instance;
 
-  static void addUser() {
-    //TODO: replace with business logic
-    final user = <String, dynamic>{
-      "firstName": "David",
-      "lastName": "Merkl",
-    };
-
-    db.collection("users").add(user).then((DocumentReference doc) =>
-        debugPrint("Successfully add document with ID: ${doc.id}"));
+  void addUser(ProfileData profileData) {
+    db
+        .collection("users")
+        .add(profileData.toJson())
+        .then((DocumentReference doc) =>
+            debugPrint("Successfully add document with ID: ${doc.id}"))
+        .onError((error, stackTrace) => stackTrace.toString());
   }
 
-  static void getUser() {
+  void getUsers() {
     db.collection("users").get().then((event) => {
           for (var doc in event.docs) {debugPrint("${doc.id} => ${doc.data()}")}
         });
