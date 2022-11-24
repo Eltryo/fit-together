3d5c8576abe61cffff5bc713bcac159516406115
Eltryo <david.merkl.dm@gmail.com>
2022-11-24 22:18:03 +0100

    restructure email creation for registration

---
+++ lib/page/create_email.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:sports_app/page/route_builder.dart';
+
+import '../utils/colors.dart';
+import '../widget/rounded_button_widget.dart';
+import 'create_password.dart';
+
+//TODO: try using ChangeNotifierProviders
+class CreateEmail extends StatefulWidget {
+  const CreateEmail({Key? key}) : super(key: key);
+
+  @override
+  State<CreateEmail> createState() => _CreateEmailState();
+}
+
+class _CreateEmailState extends State<CreateEmail> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: AppColors.appBackgroundColor,
+        body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 50.0,
+            vertical: 20.0,
+          ),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Registration",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+
+                  const Text("Please enter your email address "),
+
+                  const SizedBox(height: 10),
+
+                  //form field for email
+                  buildEmailFormField(
+                    controller: emailController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter an email";
+                      }
+                      return null;
+                    },
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  RoundedButtonWidget(
+                      text: "Next",
+                      onPressed: () {
+                        Navigator.of(context).push(RouteBuilder(
+                            pageBuilder:
+                                (context, animation, secondaryAnimation) =>
+                                    const CreatePassword()));
+                      })
+                ],
+              ),
+            )
+          ]),
+        ));
+  }
+
+  TextFormField buildEmailFormField(
+      {required TextEditingController controller, required validator}) {
+    return TextFormField(
+      controller: controller,
+      validator: validator,
+      decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: "Enter your Email",
+          contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10)),
+    );
+  }
+}
