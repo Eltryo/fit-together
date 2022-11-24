aca5f0dc384dc28d3b6f472127f81014de8d3c53
Eltryo <david.merkl.dm@gmail.com>
2022-11-24 22:16:24 +0100

    Add widget for registration submission

---
+++ lib/page/submit_registration.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SubmitRegistration extends StatefulWidget {
+  const SubmitRegistration({Key? key}) : super(key: key);
+
+  @override
+  State<SubmitRegistration> createState() => _SubmitRegistrationState();
+}
+
+class _SubmitRegistrationState extends State<SubmitRegistration> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
