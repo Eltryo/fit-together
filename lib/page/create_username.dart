bcd08c9962ddd2dcc2105d7243b1a796b20f7c64
Eltryo <david.merkl.dm@gmail.com>
2022-12-11 23:49:37 +0100

    Add logic for creating username during registration

--- lib/page/create_username.dart
+++ lib/page/create_username.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sports_app/main.dart';
 import 'package:sports_app/page/route_builder.dart';
 import 'package:sports_app/page/submit_registration.dart';
+import 'package:sports_app/utils/colors.dart';
 import 'package:sports_app/widget/rounded_button_widget.dart';
 
 class CreateUsername extends StatefulWidget {
@@ -14,68 +16,86 @@ class CreateUsername extends StatefulWidget {
 }
 
 class _CreateUsernameState extends State<CreateUsername> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController usernameController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  //TODO: dispose controller
   @override
   void dispose() {
     super.dispose();
+    usernameController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    //TODO: create scaffold
-    Provider.of<String>(context);
-    return Form(
-      key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            "Username",
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return Scaffold(
+        backgroundColor: AppColors.appBackgroundColor,
+        body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 50.0,
+            vertical: 20.0,
           ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Username",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
 
-          const Text("Please enter your username"),
+                    const Text("Please enter your username"),
 
-          const SizedBox(height: 10),
+                    const SizedBox(height: 10),
 
-          //form field for email
-          buildUsernameFormField(
-            controller: usernameController,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Please enter an username";
-              }
-              return null;
-            },
+                    //form field for email
+                    buildUsernameFormField(
+                      controller: usernameController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter an username";
+                        }
+                        return null;
+                      },
+                    ),
+
+                    const SizedBox(height: 10),
+
+                    RoundedButtonWidget(
+                        text: "Next",
+                        onPressed: () {
+                          Navigator.of(context).push(RouteBuilder(
+                              pageBuilder:
+                                  (context, animation, secondaryAnimation) =>
+                                      SubmitRegistration()));
+                        })
+                  ],
+                ),
+              ),
+            ],
           ),
-
-          const SizedBox(height: 10),
-
-          RoundedButtonWidget(
-              text: "Next",
-              onPressed: () {
-                //TODO: create user with controllers accordingly
-                Navigator.of(context).push(RouteBuilder(
-                    pageBuilder: (context, animation, secondaryAnimation) =>
-                        const SubmitRegistration()));
-              })
-        ],
-      ),
-    );
+        ));
   }
 
-  TextFormField buildUsernameFormField(
+  Consumer buildUsernameFormField(
       {required TextEditingController controller, required validator}) {
-    return TextFormField(
-      controller: controller,
-      validator: validator,
-      decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          hintText: "Enter your Username",
-          contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10)),
-    );
+    return Consumer(builder: (context, ref, child) {
+      return TextFormField(
+        controller: controller,
+        validator: validator,
+        decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: "Enter your Username",
+            contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10)),
+        onChanged: (text) {
+          ref.read(MyApp.usernameProvider.notifier).state = text;
+        },
+      );
+    });
   }
 }
