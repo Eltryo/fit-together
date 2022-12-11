3ed71e86cac52ebb12cabd6e5d8100af16bc9dce
Eltryo <david.merkl.dm@gmail.com>
2022-12-11 23:50:23 +0100

    Add logic for providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,33 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sports_app/page/create_email.dart';
-import 'package:sports_app/page/edit_profile.dart';
 import 'package:sports_app/page/splash_screen.dart';
-import 'package:sports_app/page/wrapper.dart';
+import 'package:sports_app/services/auth.dart';
 import 'package:sports_app/utils/colors.dart';
+
+import 'models/auth_user.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
+  static final authServiceProvider =
+      StreamProvider<AuthUser?>((ref) => AuthService().user);
+  static final emailProvider = StateProvider<String>((ref) => "");
+  static final passwordProvider = StateProvider((ref) => "");
+  static final usernameProvider = StateProvider((ref) => "");
+
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
-      // providers: [
-      //   StreamProvider(create: (_) => AuthService().user, initialData: null),
-      // ],
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
       child: MaterialApp(
         title: "fit-together",
         theme: ThemeData(
