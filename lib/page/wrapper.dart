b1ad91d46ee9da6a08d560e078df410359bd447d
Eltryo <david.merkl.dm@gmail.com>
2022-12-11 23:53:01 +0100

    Add logic for switching between routes on auth changes (wip)

--- lib/page/wrapper.dart
+++ lib/page/wrapper.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sports_app/main.dart';
 import 'package:sports_app/page/authenticate.dart';
+import 'package:sports_app/page/route_builder.dart';
 
-import '../models/app_user.dart';
-import '../services/provider.dart';
 import 'home.dart';
 
-class Wrapper extends ConsumerWidget {
+class Wrapper extends ConsumerStatefulWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    AppUser? appUser = ref.watch(CustomProvider.authServiceProvider
-        as ProviderListenable<AppUser?>); //TODO find better solution
+  ConsumerState<Wrapper> createState() => _WrapperState();
+}
 
-    if (appUser == null) {
-      return const Authenticate();
-    } else {
-      return const Home();
-    }
+class _WrapperState extends ConsumerState<Wrapper> {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration.zero, () {
+      final appUser = ref.watch(MyApp.authServiceProvider);
+      debugPrint("The app user is $appUser");
+
+      appUser.when(
+          data: (user) => user != null
+              ? Navigator.of(context).pushReplacement(RouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) =>
+                      const Home()))
+              : Navigator.of(context).pushReplacement(RouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) =>
+                      const Authenticate())),
+          error: (error, _) => Text("Error: $error"),
+          loading: () => const CircularProgressIndicator());
+    });
+
+    return const SizedBox.shrink();
   }
 }
