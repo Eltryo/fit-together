7c06b62f028a1e302c69204f1a5900c8c15e711a
Eltryo <david.merkl.dm@gmail.com>
2022-12-26 12:48:50 +0100

    Add wrapper logic

--- lib/page/wrapper.dart
+++ lib/page/wrapper.dart
@@ -1,43 +1,30 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:sports_app/main.dart';
-// import 'package:sports_app/page/authenticate.dart';
-// import 'package:sports_app/page/route_builder.dart';
-//
-// import 'home.dart';
-//
-// class Wrapper extends ConsumerStatefulWidget {
-//   const Wrapper({Key? key}) : super(key: key);
-//
-//   @override
-//   ConsumerState<Wrapper> createState() => _WrapperState();
-// }
-//
-// class _WrapperState extends ConsumerState<Wrapper> {
-//   @override
-//   Widget build(BuildContext context) {
-//     // Future.delayed(Duration.zero, () {
-//     //   final appUser = ref.watch(MyApp.authServiceProvider);
-//     //   debugPrint("The app user is ${appUser.value.toString()}");
-//     //
-//     //   appUser.when(
-//     //       data: (user) => user != null
-//     //           ? Navigator.of(context).pushReplacement(RouteBuilder(widget: const Home()).buildRoute())
-//     //           : Navigator.of(context).pushReplacement(RouteBuilder(widget: const Authenticate()).buildRoute()),
-//     //       error: (error, _) => Text("Error: $error"),
-//     //       loading: () => const CircularProgressIndicator());
-//     // });
-//     //
-//     // return const SizedBox.shrink();
-//
-//     final appUser = ref.watch(MyApp.authServiceProvider);
-//     debugPrint("The app user is ${appUser.value.toString()}");
-//
-//     return appUser.when(
-//         data: (user) => user != null
-//               ? const Home()
-//               : const Authenticate(),
-//         error: (error, _) => Text("Error: $error"),
-//         loading: () => const CircularProgressIndicator());
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../main.dart';
+import 'authenticate.dart';
+import 'home.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(builder: (context, ref, child) {
+      final user = ref.watch(MyApp.authStreamProvider);
+
+      return user.when(
+          data: (data) {
+            if (data != null) {
+              debugPrint("user is logged in");
+              return const Home();
+            } else {
+              debugPrint("user is logged out");
+              return const Authenticate();
+            }
+          },
+          error: (error, _) => Text("Error: $error"),
+          loading: () => const CircularProgressIndicator());
+    });
+  }
+}
