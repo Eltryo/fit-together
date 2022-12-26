10ee56c5c270f20d0531e0d52aed969d05a93957
Eltryo <david.merkl.dm@gmail.com>
2022-12-26 12:42:52 +0100

    Add logic for authentication method decision

--- lib/page/authenticate.dart
+++ lib/page/authenticate.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sports_app/page/create_email.dart';
 import 'package:sports_app/page/sign_in.dart';
 
@@ -9,10 +10,32 @@ class Authenticate extends StatefulWidget {
   State<Authenticate> createState() => _AuthenticateState();
 }
 
-//TODO: remove unnecessary widget
 class _AuthenticateState extends State<Authenticate> {
+  Widget currentWidget = const Scaffold();
+
   @override
   Widget build(BuildContext context) {
-    return const SignIn();
+    return currentWidget;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    route();
+  }
+
+  Future route() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool? firstTime = prefs.getBool("first_time");
+    if (firstTime != null && !firstTime) {
+      setState(() {
+        currentWidget = const SignIn();
+      });
+    } else {
+      prefs.setBool("first_time", false);
+      setState(() {
+        currentWidget = const CreateEmail();
+      });
+    }
   }
 }
