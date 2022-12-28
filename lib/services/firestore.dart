b7125ea3e98a191be99c5f303cb4ef6a368a8282
Eltryo <david.merkl.dm@gmail.com>
2022-12-28 18:03:13 +0100

    Add logic for retrieving all users and specific user from database

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -12,13 +12,29 @@ class FirestoreService {
         .add(profileData.toJson())
         .then((DocumentReference doc) =>
             debugPrint("Successfully add document with ID: ${doc.id}"))
-        .onError((error, stackTrace) => stackTrace.toString());
+        .onError((error, _) => debugPrint("Error: $error"));
   }
 
-  void getUsers() {
-    db.collection("users").get().then((collection) => {
-          for (var doc in collection.docs)
-            {debugPrint("${doc.id} => ${doc.data()}")}
-        });
+  Future<Iterable<AppUser>> getUsers() async {
+    return db
+        .collection("users")
+        .withConverter(
+            fromFirestore: AppUser.fromJson,
+            toFirestore: (AppUser appUser, _) => appUser.toJson())
+        .get()
+        .then((colSnap) => colSnap.docs.map((docSnap) => docSnap.data()),
+            onError: (error) => debugPrint("Error: $error"));
+  }
+
+  Future<AppUser> getUserByUID(String id) {
+    return db
+        .collection("users")
+        .withConverter(
+            fromFirestore: AppUser.fromJson,
+            toFirestore: (AppUser appUser, _) => appUser.toJson())
+        .where("uid", isEqualTo: id)
+        .get()
+        .then((value) => value.docs.map((docSnap) => docSnap.data()).single,
+            onError: (error) => debugPrint("Error: $error"));
   }
 }
