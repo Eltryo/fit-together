a8dfb9cd3c848fa2989581c65caad2f6694bd99f
Eltryo <david.merkl.dm@gmail.com>
2023-01-24 23:51:51 +0100

    Delete app user stats from user model

--- lib/models/app_user.dart
+++ lib/models/app_user.dart
@@ -8,7 +8,7 @@ class AppUser {
   final String? firstName;
   final String? lastName;
   final String? imageUrl;
-  final AppUserStats appUserStats;
+  // final AppUserStats? appUserStats;
 
   AppUser(
       {required this.uid,
@@ -17,7 +17,8 @@ class AppUser {
       this.firstName,
       this.lastName,
       this.imageUrl,
-      this.appUserStats = const AppUserStats()});
+      // this.appUserStats = const AppUserStats()
+      });
 
   factory AppUser.fromJson(DocumentSnapshot<Map<String, dynamic>> snapshot,
       SnapshotOptions? options) {
@@ -29,7 +30,7 @@ class AppUser {
         firstName: data?["firstName"],
         lastName: data?["lastName"],
         imageUrl: data?["imageUrl"],
-        appUserStats: data?["appUserStats"]
+        // appUserStats: data?["appUserStats"]
     );
   }
 
@@ -38,13 +39,10 @@ class AppUser {
       "uid": uid,
       "username": username,
       "email": email,
-      "appUserStats": appUserStats,
+      // "appUserStats": appUserStats,
       if (firstName != null) "firstName": firstName,
       if (lastName != null) "lastName": lastName,
       if (imageUrl != null) "imageUrl": imageUrl
     };
   }
-
-  @override
-  List<Object?> get props => [username, firstName, lastName, email, imageUrl];
 }
