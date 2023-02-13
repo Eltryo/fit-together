23f1471aed4cddec4a83e553def6d50d5f6983fe
Eltryo <david.merkl.dm@gmail.com>
2023-02-13 19:34:28 +0100

    Add shimmering loading effect (wip)

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:fit_together/providers.dart';
+import 'package:fit_together/widgets/profile_page_posts.dart';
+import 'package:fit_together/widgets/shimmer_loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -16,38 +18,73 @@ class _ProfilePageState extends ConsumerState<ProfilePage> {
   String _username = "";
   String _email = "";
   String? _imageUrl;
+  late bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    fetchAppUser();
+    fetchAppUserData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 40,
-        ),
-        ProfileImage(
-          imagePath: _checkImagePath(_imageUrl),
-          onClicked: () {},
-        ),
-        const SizedBox(height: 10),
-        Text(
-          _username,
-          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        Text(
-          _email,
-          style:
-              const TextStyle(fontStyle: FontStyle.italic, color: Colors.grey),
-        ),
-        const SizedBox(height: 20),
-        const ProfileStats(),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          ProfileImage(
+            imagePath: _checkImagePath(_imageUrl),
+            onClicked: () {},
+          ),
+          const SizedBox(height: 10),
+          ShimmerLoading(
+            isLoading: _isLoading,
+            child: buildText(
+              Text(
+                  _username,
+                  style:
+                  const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+              ),
+            ),
+          ),
+          ShimmerLoading(
+            isLoading: _isLoading,
+            child: buildText(
+              Text(
+                _email,
+                style: const TextStyle(
+                    fontStyle: FontStyle.italic, color: Colors.grey)
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          const ProfileStats(),
+          const SizedBox(height: 20),
+          const ProfilePagePosts()
+        ],
+      ),
     );
+  }
+
+  Widget buildText(Text text) {
+    if (_isLoading) {
+      return Container(
+        padding: const EdgeInsets.only(bottom: 1),
+        width: MediaQuery
+            .of(context)
+            .size
+            .width / 2,
+        height: 15, //TODO change hardcoded height
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(16),
+        ),
+      );
+    } else {
+      return text;
+    }
   }
 
   String _checkImagePath(String? imagePath) {
@@ -57,16 +94,17 @@ class _ProfilePageState extends ConsumerState<ProfilePage> {
     return imagePath;
   }
 
-  void fetchAppUser() {
+  void fetchAppUserData() {
     final authService = ref.read(authServiceProvider);
     final firestoreService = ref.read(firestoreServiceProvider);
     if (authService.currentUid != null) {
       firestoreService.getUserByUid(authService.currentUid!).then((appUser) {
         setState(
-          () {
+              () {
             _username = appUser.username;
             _email = appUser.email;
             _imageUrl = appUser.imageUrl;
+            _isLoading = false;
           },
         );
       }, onError: (error) => debugPrint("Error: $error"));
