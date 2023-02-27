27a9418e39e8389310cd25a0403bee26642adb05
Eltryo <david.merkl.dm@gmail.com>
2023-02-27 11:57:16 +0100

    Add icons to image dialog

--- lib/widgets/image_dialog.dart
+++ lib/widgets/image_dialog.dart
@@ -1,36 +1,86 @@
 import 'dart:typed_data';
 
+import 'package:fit_together/screens/post_comments.dart';
 import 'package:fit_together/service_locator.dart';
 import 'package:fit_together/services/authentication.dart';
 import 'package:fit_together/services/firestore.dart';
 import 'package:flutter/material.dart';
 
-class ImageDialog extends StatelessWidget {
+class ImageDialog extends StatefulWidget {
   final Uint8List imageData;
 
   const ImageDialog({required this.imageData, Key? key}) : super(key: key);
 
   @override
+  State<ImageDialog> createState() => _ImageDialogState();
+}
+
+class _ImageDialogState extends State<ImageDialog> {
+  final firestoreService = locator<FirestoreService>();
+  final authenticationService = locator<AuthenticationService>();
+
+  bool _liked = false;
+
+  @override
   Widget build(BuildContext context) {
-    final firestoreService = locator<FirestoreService>();
-    final authenticationService = locator<AuthenticationService>();
     final appUserFuture =
         firestoreService.getUserById(authenticationService.currentUid!);
-
     return Dialog(
       child: Container(
         width: 400,
         height: 400,
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: MemoryImage(imageData),
+            image: MemoryImage(widget.imageData),
             fit: BoxFit.cover,
           ),
           borderRadius: const BorderRadius.all(
             Radius.circular(3),
           ),
         ),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      _toggle();
+                    },
+                    icon: Icon(
+                      _liked ? Icons.favorite : Icons.favorite_border,
+                      color: Colors.red,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const PostComments(),
+                        ),
+                      );
+                    },
+                    icon: const Icon(
+                      Icons.send,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
+    );
+  }
+
+  void _toggle() {
+    setState(
+      () {
+        _liked = !_liked;
+      },
     );
   }
 }
