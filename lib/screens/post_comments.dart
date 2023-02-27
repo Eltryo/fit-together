65caab1d7171266d962cbfeff063ca7b1dee9637
Eltryo <david.merkl.dm@gmail.com>
2023-02-27 11:57:49 +0100

    Add scaffold of comments section

---
+++ lib/screens/post_comments.dart
@@ -0,0 +1,50 @@
+import 'package:fit_together/service_locator.dart';
+import 'package:fit_together/services/authentication.dart';
+import 'package:fit_together/services/firestore.dart';
+import 'package:flutter/material.dart';
+
+class PostComments extends StatefulWidget {
+  const PostComments({Key? key}) : super(key: key);
+
+  @override
+  State<PostComments> createState() => _PostCommentsState();
+}
+
+class _PostCommentsState extends State<PostComments> {
+  final firestoreService = locator<FirestoreService>();
+  final authenticationService = locator<AuthenticationService>();
+
+  @override
+  Widget build(BuildContext context) {
+    final appUserFuture =
+        firestoreService.getUserById(authenticationService.currentUid!);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Column(
+          children: [
+            const Text("Comments"),
+            FutureBuilder(
+              future: appUserFuture,
+              builder: (context, snapshot) {
+                //TODO: handle error
+                if (snapshot.hasData) {
+                  return Text(
+                    snapshot.data!.username,
+                    style: const TextStyle(fontSize: 14, color: Colors.grey),
+                  );
+                }
+                return const CircularProgressIndicator();
+              },
+            )
+          ],
+        ),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          //TODO: view comments
+        },
+      ),
+    );
+  }
+}
