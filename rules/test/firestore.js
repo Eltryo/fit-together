d94a4b72aeaf896ee339a4e4fecd6c720eb76d3a
Eltryo <david.merkl.dm@gmail.com>
2023-04-21 00:23:37 +0200

    Fix firestore rules tests

--- rules/test/firestore.js
+++ rules/test/firestore.js
@@ -1,19 +1,17 @@
 const firebase = require("@firebase/testing");
-// const sinon = require("sinon")
 
 const FIREBASE_PROJECT_ID = "fit-together-74181";
 const myAuthUid = "cVV9YzdhqTSH0vr4YI4IzhzjroH3";
 const otherAuthUid = "fW6YDKz1uwhAglNhF8Lb6HKqzwm2";
-const myAuth = {uid: myAuthUid, email: "david.merkl.dm@gmail.com"};
+const myAuth = {uid: myAuthUid, email: "david.merkl.dm@gmail.com", firebase: {sign_in_provider: "password"}};
 const otherAuth = {uid: otherAuthUid, email: "max.mustermann@gmail.com"};
 
 describe("firestore tests", () => {
-    function getFireStore(myAuth, data = null) {
+    async function setup(myAuth, data = null) {
         const db = firebase.initializeTestApp({projectId: FIREBASE_PROJECT_ID, auth: myAuth,}).firestore()
         if (data) {
             for (const key in data) {
-                const ref = db.doc(key)
-                ref.set(data[key])
+                await db.doc(key).set(data[key])
             }
         }
 
@@ -25,69 +23,134 @@ describe("firestore tests", () => {
     })
 
     describe("users collection tests", () => {
-        it('should get user document', async () => {
-                const db = getFireStore(myAuth)
-                const doc = db.collection("users").doc("cVV9YzdhqTSH0vr4YI4IzhzjroH3")
-                await firebase.assertSucceeds(doc.get());
-            }
-        );
+        describe("get request", () => {
+            it('should not get user document from false user', async () => {
+                    const db = await setup(myAuth)
+                    const doc = db.collection("users").doc(otherAuthUid)
+                    await firebase.assertFails(doc.get());
+                }
+            );
 
-        it('should not get user document from false user', async () => {
-                const db = getFireStore(myAuth)
-                const doc = db.collection("users").doc("fW6YDKz1uwhAglNhF8Lb6HKqzwm2")
-                await firebase.assertFails(doc.get());
-            }
-        );
+            it('should not get user document without authentication', async () => {
+                    const db = await setup(null)
+                    const doc = db.collection("users").doc(otherAuthUid)
+                    await firebase.assertFails(doc.get());
+                }
+            );
 
-        it('should not get user document without authentication', async () => {
-                const db = getFireStore(myAuth)
-                const doc = db.collection("users").doc("fW6YDKz1uwhAglNhF8Lb6HKqzwm2")
-                await firebase.assertFails(doc.get());
-            }
-        );
+            it('should get public user document', async () => {
+                    const userDocPath = `users/${otherAuthUid}`
+                    const mockData = {
+                        [userDocPath]: {
+                            username: "Eltryo",
+                            email: "david.merkl.dm@gmail.com",
+                            appUserStats: {},
+                            visibility: "public"
+                        }
+                    }
+                    const db = await setup(myAuth, mockData)
+                    const doc = db.collection("users").doc(otherAuthUid)
+                    await firebase.assertSucceeds(doc.get());
+                }
+            );
 
-        it('should not create user document without authentication', async () => {
-                const db = getFireStore(null)
-                const doc = db.collection("users").doc("cVV9YzdhqTSH0vr4YI4IzhzjroH3")
-                await firebase.assertFails(doc.set({foo: "bar"}));
-            }
-        );
-    })
+            it('should get user document', async () => {
+                    const db = await setup(myAuth)
+                    const doc = db.collection("users").doc(myAuthUid)
+                    await firebase.assertSucceeds(doc.get());
+                }
+            );
+        })
 
-    describe("posts collection tests", () => {
-        //TODO: write more tests
-        it('should not get user document without authentication', async () => {
-                const db = getFireStore(null)
-                const doc = db.collection("posts").doc("doc")
-                await firebase.assertFails(doc.get());
-            }
-        );
-
-        it('should get own user document', async () => {
-                const db = getFireStore(myAuth)
-                const doc = db.collection("posts").where("ownerId", "==", myAuthUid)
-                await firebase.assertSucceeds(doc.get());
-            }
-        );
-
-        it('should get public user document', async () => {
-                //TODO: fix failing test
-                const mockData = {
-                    myAuthUid: {
+        describe("post request", () => {
+            it('should not create user document without authentication', async () => {
+                    const db = await setup(null)
+                    const doc = db.collection("users").doc(myAuthUid)
+                    await firebase.assertFails(doc.set({
                         username: "Eltryo",
                         email: "david.merkl.dm@gmail.com",
                         appUserStats: {},
                         visibility: "public"
-                    }
+                    }));
                 }
-                const db = getFireStore(myAuth, mockData)
-                // const userDoc = await db.collection("users").doc(otherAuthUid).get()
-                // sinon.stub(userDoc, "exists").returns(true)
-                // sinon.stub(userDoc, "get").withArgs("visibility").returns("public")
-                const doc = db.collection("posts")
-                    .where("ownerId", "==", otherAuthUid)
-                await firebase.assertFails(doc.get());
-            }
-        );
+            );
+
+            it('should not create user document with false fields', async () => {
+                    const db = await setup(myAuth)
+                    const doc = db.collection("users").doc(myAuthUid)
+                    await firebase.assertFails(doc.set({foo: "bar"}));
+                }
+            );
+
+            it('should not create user document with false field types', async () => {
+                    const db = await setup(myAuth)
+                    const doc = db.collection("users").doc(myAuthUid)
+                    await firebase.assertFails(doc.set({
+                        username: "Eltryo",
+                        email: "david.merkl.dm@gmail.com",
+                        appUserStats: "",
+                        visibility: "public"
+                    }));
+                }
+            );
+
+            it('should not create user document with false fields', async () => {
+                    const auth = {
+                        uid: myAuthUid,
+                        email: "david.merkl.dm@gmail.com",
+                        "firebase.sign_in_provider": "anonymous"
+                    }
+                    const db = await setup(auth)
+                    const doc = db.collection("users").doc(myAuthUid)
+                    await firebase.assertFails(doc.set({
+                        username: "Eltryo",
+                        email: "david.merkl.dm@gmail.com",
+                        appUserStats: {},
+                        visibility: "public"
+                    }));
+                }
+            );
+        })
+    })
+
+    describe("posts collection tests", () => {
+        //TODO: write more tests
+        describe("get request", () => {
+            it('should not get user document without authentication', async () => {
+                    const db = await setup(null)
+                    const doc = db.collection("posts").doc("doc")
+                    await firebase.assertFails(doc.get());
+                }
+            );
+
+            it('should get own user document', async () => {
+                    const db = await setup(myAuth)
+                    const doc = db.collection("posts").where("ownerId", "==", myAuthUid)
+                    await firebase.assertSucceeds(doc.get());
+                }
+            );
+
+            it('should get public user document', async () => {
+                    const userDocPath = `users/${otherAuthUid}`
+                    const postDocPath = `posts/ `
+                    const mockData = {
+                        [userDocPath]: {
+                            username: "Eltryo",
+                            email: "david.merkl.dm@gmail.com",
+                            appUserStats: {},
+                            visibility: "public"
+                        },
+                        [postDocPath]: {
+                            path: "",
+                            ownerId: otherAuthUid
+                        }
+                    }
+                    const db = await setup(myAuth, mockData)
+                    const doc = db.collection("posts")
+                        .where("ownerId", "==", otherAuthUid)
+                    await firebase.assertSucceeds(doc.get());
+                }
+            );
+        })
     })
 })
